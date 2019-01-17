d47548920468211063f4ecca8ce985a26dc2ba78
Molly McCarron <mccarronmollye@gmail.com>
2019-01-17 11:50:28 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Molly McCarron"
+  end
+
+  get '/hometown' do
+  "My hometown is Red Bank, New Jersey"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is Thunder Road By Bruce Springsteen"
+  end
 end
